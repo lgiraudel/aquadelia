11d87c1a2f5d1bdf58d77293f6d9b06109478e51
Loïc Giraudel <l.giraudel@purch.com>
2016-01-31 18:35:36 +0100

    Fix path in galleries page

--- js/galleries.js
+++ js/galleries.js
@@ -3,8 +3,8 @@ $(function() {
         var galeriesHtml = '';
         $(json.galeries).each(function(i, galerieInfo) {
             galeriesHtml += '<li class="galerie">' +
-                '<a href="/pages/galerie.html?galerie=' + galerieInfo.dir + '" class="clickable-area">' +
-                '<img src="/galeries/' + galerieInfo.thumb + '" class="thumb"/>' +
+                '<a href="galerie.html?galerie=' + galerieInfo.dir + '" class="clickable-area">' +
+                '<img src="../galeries/' + galerieInfo.thumb + '" class="thumb"/>' +
                 '<div class="title">' + galerieInfo.title + '</div>' +
                 '</a>' +
                 '<div class="description">' + galerieInfo.description + '</div>' +
